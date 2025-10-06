80c529eaba694bca3fd8ede5ad446dfb9cdf83ff
Rixshi04 <s.rishikumar04@gmail.com>
2025-10-06 09:51:16 +0530

    frontend main code

---
+++ App.tsx
@@ -0,0 +1,33 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Demo from "./pages/Demo";
+import Technology from "./pages/Technology";
+import Resources from "./pages/Resources";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+        <Sonner />
+      </TooltipProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
+
+export default App;
